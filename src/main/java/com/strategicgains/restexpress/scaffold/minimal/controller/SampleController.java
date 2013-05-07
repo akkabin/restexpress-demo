7a55c5068f502e76bd8a9747348af48d80a922c8
brianchen112 <briangxchen@gmail.com>
2013-05-07 21:12:36 +1000

    restexpress demo

---
+++ src/main/java/com/strategicgains/restexpress/scaffold/minimal/controller/SampleController.java
@@ -0,0 +1,41 @@
+package com.strategicgains.restexpress.scaffold.minimal.controller;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+
+import com.strategicgains.hyperexpress.domain.LinkableCollection;
+import com.strategicgains.restexpress.Request;
+import com.strategicgains.restexpress.Response;
+
+@Path("/sample")
+public interface SampleController
+{
+		
+	@Path("/your/collection/route/here.{format}")
+	@POST
+	public Object create(Request request, Response response);
+	
+
+	@Path("/your/route/here/{sampleId}.{format}")
+	@GET
+	public Object read(Request request, Response response);
+	
+
+	@Path("/your/collection/route/here.{format}")
+	@GET
+	public LinkableCollection<Object> readAll(Request request, Response response);
+	
+
+	@Path("/your/route/here/{sampleId}.{format}")
+	@PUT
+	public void update(Request request, Response response);
+	
+
+	@Path("/your/route/here/{sampleId}.{format}")
+	@DELETE
+	public void delete(Request request, Response response);
+	
+}
---
+++ src/main/java/com/strategicgains/restexpress/scaffold/minimal/controller/SampleControllerImpl.java
@@ -0,0 +1,72 @@
+package com.strategicgains.restexpress.scaffold.minimal.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.strategicgains.hyperexpress.domain.LinkableCollection;
+import com.strategicgains.restexpress.Request;
+import com.strategicgains.restexpress.Response;
+import com.strategicgains.restexpress.scaffold.minimal.config.MetricsConfig;
+
+
+public class SampleControllerImpl implements SampleController
+{
+	private static final Logger LOG = LoggerFactory.getLogger(SampleControllerImpl.class);
+	public SampleControllerImpl()
+	{
+		super();
+	}
+
+	
+	public Object create(Request request, Response response)
+	{
+		//TODO: Your 'create' logic here...
+		//Sample order = request.getBodyAs(Sample.class, "Sample details not provided");
+		return null;
+	}
+
+	public Object read(Request request, Response response)
+	{
+		//TODO: Your 'read' logic here...
+		Map<String, String> queryMap = request.getQueryStringMap();
+		String id = request.getHeader("sampleId", "No Sample ID supplied");
+		LOG.debug("URL para: " + id);
+		LOG.debug("Query map: " + queryMap.toString());
+		
+		return queryMap;
+	}
+
+	public LinkableCollection<Object> readAll(Request request, Response response)
+	{
+		//TODO: Your 'readAll' logic here...
+		System.out.println("Are you readAll of me");
+		List<Object> res = new ArrayList<Object>();
+		res.add("First");
+		res.add("Second");
+		res.add(new Date());
+		
+		
+		
+		return new LinkableCollection(res);
+	}
+
+	public void update(Request request, Response response)
+	{
+		//TODO: Your 'update' logic here...
+		//String id = request.getHeader(Constants.Url.SAMPLE_ID, "No Sample ID supplied");
+		response.setResponseNoContent();
+	}
+
+	public void delete(Request request, Response response)
+	{
+		//TODO: Your 'delete' logic here...
+		//String id = request.getHeader(Constants.Url.SAMPLE_ID, "No Sample ID supplied");
+		response.setResponseNoContent();
+	}
+}
