005e7704f863a83187ea108d3d1a6669bfccc00b
brianchen112 <briangxchen@gmail.com>
2013-05-18 09:24:15 +1000

    clean

--- src/main/java/com/strategicgains/restexpress/scaffold/minimal/controller/SampleController.java
+++ src/main/java/com/strategicgains/restexpress/scaffold/minimal/controller/SampleController.java
@@ -6,6 +6,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.strategicgains.hyperexpress.domain.LinkableCollection;
 import com.strategicgains.restexpress.Request;
@@ -41,7 +42,7 @@ public interface SampleController
 	
 	@Path("/your/route/string/abc.{format}")
 	@GET
-	@Produces("text/plain")
+	@Produces(MediaType.TEXT_PLAIN)
 	public String readAsString(Request request, Response response);
 	
 }
--- src/main/java/com/strategicgains/restexpress/scaffold/minimal/controller/SampleControllerImpl.java
+++ src/main/java/com/strategicgains/restexpress/scaffold/minimal/controller/SampleControllerImpl.java
@@ -12,63 +12,55 @@ import com.strategicgains.hyperexpress.domain.LinkableCollection;
 import com.strategicgains.restexpress.Request;
 import com.strategicgains.restexpress.Response;
 
+public class SampleControllerImpl implements SampleController {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(SampleControllerImpl.class);
 
-public class SampleControllerImpl implements SampleController
-{
-	private static final Logger LOG = LoggerFactory.getLogger(SampleControllerImpl.class);
-	public SampleControllerImpl()
-	{
+	public SampleControllerImpl() {
 		super();
 	}
 
-	
-	public Object create(Request request, Response response)
-	{
-		//TODO: Your 'create' logic here...
-		//Sample order = request.getBodyAs(Sample.class, "Sample details not provided");
+	public Object create(Request request, Response response) {
+		// TODO: Your 'create' logic here...
+		// Sample order = request.getBodyAs(Sample.class,
+		// "Sample details not provided");
 		return null;
 	}
 
-	public Object read(Request request, Response response)
-	{
-		//TODO: Your 'read' logic here...
+	public Object read(Request request, Response response) {
+		// TODO: Your 'read' logic here...
 		Map<String, String> queryMap = request.getQueryStringMap();
 		String id = request.getHeader("sampleId", "No Sample ID supplied");
 		LOG.debug("URL para: " + id);
 		LOG.debug("Query map: " + queryMap.toString());
-		
+
 		return queryMap;
 	}
 
-	public LinkableCollection<Object> readAll(Request request, Response response)
-	{
-		//TODO: Your 'readAll' logic here...
+	public LinkableCollection<Object> readAll(Request request, Response response) {
+		// TODO: Your 'readAll' logic here...
 		System.out.println("Are you readAll of me");
 		List<Object> res = new ArrayList<Object>();
 		res.add("First");
 		res.add("Second");
 		res.add(new Date());
-		
-		
-		
+
 		return new LinkableCollection(res);
 	}
 
-	public void update(Request request, Response response)
-	{
-		//TODO: Your 'update' logic here...
-		//String id = request.getHeader(Constants.Url.SAMPLE_ID, "No Sample ID supplied");
+	public void update(Request request, Response response) {
+		// TODO: Your 'update' logic here...
+		// String id = request.getHeader(Constants.Url.SAMPLE_ID,
+		// "No Sample ID supplied");
 		response.setResponseNoContent();
 	}
 
-	public void delete(Request request, Response response)
-	{
-		//TODO: Your 'delete' logic here...
-		//String id = request.getHeader(Constants.Url.SAMPLE_ID, "No Sample ID supplied");
+	public void delete(Request request, Response response) {
+		// TODO: Your 'delete' logic here...
+		// String id = request.getHeader(Constants.Url.SAMPLE_ID,
+		// "No Sample ID supplied");
 		response.setResponseNoContent();
 	}
-
-
 
 	public String readAsString(Request request, Response response) {
 		// TODO Auto-generated method stub
